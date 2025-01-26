64c64913876a0566ba69403fd895c1ca7a9c62c5
raj <razbc12@gmail.com>
2025-01-26 14:41:58 +0545

    Remove unnecessary code, Hard-code defualt project

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@
 import { projectContainer } from "./project-container";
 // import { format, compareAsc } from "date-fns";
 
-
-
-projectContainer.addProjectToContainer('tax', 'how to evade');
-projectContainer.addProjectToContainer('home', 'yeas');
-projectContainer.addTodoToProject(0, 'aa', 'd' , 1 , 3, 'aadasd');
-projectContainer.addTodoToProject(0, 'aaasd', 'ddsd' , 1 , 3, 'aadasd');
-projectContainer.addTodoToProject(1, 'aa', 'd' , 1 , 3, 'aadasd');
-projectContainer.setToHigh(0, 0)
-projectContainer.setToMedium(0, 1)
+projectContainer.addProjectToContainer('Default', 'All unlisted tasks are added to this project.')
 
 projectContainer.viewArray();
